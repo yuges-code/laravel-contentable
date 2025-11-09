4004b4a96b68e5c441d3699db5ab645a9f185ed5
Georgy <goshasafonov@yandex.ru>
2025-11-09 19:35:58 +0300

    test

--- tests/src/Stubs/Blocks/ParagraphData.php
+++ tests/src/Stubs/Blocks/ParagraphData.php
@@ -2,15 +2,23 @@
 
 namespace Yuges\Contentable\Tests\Stubs\Blocks;
 
+use Yuges\Contentable\Abstracts\BlockData;
 use Yuges\Contentable\Tests\Stubs\Enums\BlockType;
 
-class ParagraphData extends \Yuges\Contentable\Data\Blocks\ParagraphData
+class ParagraphData extends BlockData
 {
-    protected const BlockType TYPE = BlockType::Delimiter;
+    protected const BlockType TYPE = BlockType::Paragraph;
 
     public function __construct(
         public ?string $text = '',
     ) {
-        parent::__construct($text);
+        parent::__construct();
+    }
+
+    public function getData(): array
+    {
+        return [
+            'text' => $this->text,
+        ];
     }
 }
